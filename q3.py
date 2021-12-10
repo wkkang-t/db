9c72ed4fef494fa8f66325ea4ef7a12cbad18162
wkkang-t <kaikang_wong@mymail.sutd.edu.sg>
2021-12-10 11:27:53 +0800

    Update q3.py

--- q3.py
+++ q3.py
@@ -9,21 +9,18 @@ import ast
 spark = SparkSession.builder.appName("Assigment 2 Question 3").getOrCreate()
 # YOUR CODE GOES BELOW
 
-inputRdd = spark.read.option("header", True).option("inferSchema", "true")\
+rdd1 = spark.read.option("header", True).option("inferSchema", "true")\
 .csv("data/TA_restaurants_curated_cleaned.csv").rdd
-
 def extract_review_date_pair(line):
     ls = ast.literal_eval(line[8]) # convert string to list
-    reviews_ls = ls[0]
-    dates_ls = ls[1]
-    answer = []
-    for review, date in zip(reviews_ls, dates_ls):
-        answer.append((line[10], review, date))
-    return answer
+    reviews = ls[0]
+    dates = ls[1]
+    review_date = []
+    for review, date in zip(reviews, dates):
+        review_date.append((line[10], review, date))
+    return review_date
   
-results = inputRdd.flatMap(extract_review_date_pair)
-
-dfWithSchema = spark.createDataFrame(results).toDF("ID_TA", "review", "date")
-
-dfWithSchema.write.option("header", True)\
+rdd1 = rdd1.flatMap(extract_review_date_pair)
+df = spark.createDataFrame(rdd1).toDF("ID_TA", "review", "date")
+df.write.option("header", True)\
 .csv("q3")
